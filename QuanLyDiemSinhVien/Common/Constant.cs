755770d5859f35d6fefd545de22b4c288640259e
lequocdien <lequocdien1998@gmail.com>
2019-12-22 23:49:32 +0700

    update something

--- QuanLyDiemSinhVien/Common/Constant.cs
+++ QuanLyDiemSinhVien/Common/Constant.cs
@@ -10,13 +10,11 @@ namespace Common
     {
         //public const string SERVER_NAME = "DESKTOP-EI49O32\\TRANQUANGCHAU";
         //public const string DATABASE_NAME = "QLDSV";
-
         //public const string REMOTE_LOGIN = "HTKN";
         //public const string REMOTE_PASSWORD = "12345";
 
         public const string SERVER_NAME = "DIEN-PC";
         public const string DATABASE_NAME = "CSDLPT_QLDSV";
-        
         public const string REMOTE_LOGIN = "HTKN";
         public const string REMOTE_PASSWORD = "12345";
 
